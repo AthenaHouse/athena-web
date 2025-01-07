5133eb3707afcfc4600f13ad0a9e78d4a30ebda7
Bartosz Dziurowicz <bartosz.dziurowicz@gmail.com>
2025-01-07 08:03:52 +0100

    create tempaltes of main components

---
+++ src/components/Main/Academy/Academy.styles.jsx
@@ -0,0 +1,5 @@
+import { styled } from "styled-components";
+
+export const $Academy = styled.div`
+    color: red;
+`
---
+++ src/components/Main/Software/Software.jsx
@@ -0,0 +1,6 @@
+import { $Software } from "./Software.styles.jsx";
+
+export default function Software() {
+
+    return (<$Software>Software</$Software>);
+}
