13e2d8120d11e90f0b1323463d4b903f6b1f9bb0
Bartosz Dziurowicz <bartosz.dziurowicz@gmail.com>
2025-01-17 08:36:32 +0100

    adjust for mobiel devices

--- src/components/Banner/Logo/Logo.styles.jsx
+++ src/components/Banner/Logo/Logo.styles.jsx
@@ -3,6 +3,10 @@ import { styled } from "styled-components";
 export const $Logo = styled.img`
   height: 100%;
   user-select: none;
+
+  @media (max-width: 1024px) { 
+    height: 75px;
+  }
 `;
 
 export default $Logo;
--- src/components/Main/Home/Home.styles.jsx
+++ src/components/Main/Home/Home.styles.jsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
-import MyImage from '../../../assets/my_image_web_no_bg.jpg';
 import { darkgold, steelblue, steelgrey } from '../../../utils/colors.styles';
 
 export const $Home = styled.div`
   display: flex;
   justify-content: space-between;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export const $Content = styled.div`
@@ -18,14 +23,38 @@ export const $Content = styled.div`
     word-wrap: break-word;
     margin-bottom: 30px;
   }
+
+  @media (max-width: 768px) {
+    align-items: center;
+    text-align: center;
+
+    & p {
+      font-size: 0.9rem;
+      margin-bottom: 20px;
+    }
+  }
 `;
 
 export const $Span = styled.span`
   font-size: ${({ $motto }) => $motto ? '1.8rem' : '1.6rem'};
   color: ${({ $motto }) => $motto ? steelblue : darkgold};
   font-weight: bold;
+
+  @media (max-width: 768px) {
+    font-size: ${({ $motto }) => $motto ? '1.6rem' : '1.4rem'};
+  }
 `;
+
 
 export const $Photo = styled.div`
   display: flex;
-`
+  justify-content: center;
+  align-items: flex-start;
+
+  & img {
+    @media (max-width: 1024px) {
+      width: 350px;
+      height: auto;
+    }
+  }
+`;
