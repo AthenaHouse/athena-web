94fad3cf7a9e9bf17f08895afc28343a92881cc0
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2025-08-03 10:39:31 +0200

    Create InProgress component and implement it in Project section

---
+++ src/components/Main/InProgress/InProgress.styles.jsx
@@ -0,0 +1,43 @@
+import { styled } from "styled-components";
+import {
+  darkgold,
+  lightgold,
+  steelblue,
+  steelgrey,
+} from "../../../utils/colors.styles";
+
+export const $IconBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+
+  .icon {
+    color: ${darkgold};
+    position: absolute;
+    font-size: 200px;
+    opacity: 0.15;
+  }
+`;
+
+export const $InProgress = styled.div`
+  z-index: 1;
+  min-height: 230px;
+  border: 3px solid ${darkgold};
+  border-radius: 10px;
+  padding: 20px;
+  margin: 10px;
+
+  & p {
+    color: ${steelgrey};
+  }
+
+  & h1 {
+    color: ${steelblue};
+  }
+
+  @media (max-width: 1100px) {
+    min-height: 0;
+  }
+`;
--- src/components/Main/Project/Project.jsx
+++ src/components/Main/Project/Project.jsx
@@ -1,7 +1,11 @@
 import { $Project } from "./Project.styles.jsx";
+import InProgress from "../InProgress/InProgress.jsx";
 
 /* eslint-disable react/jsx-pascal-case */
 export default function Project() {
-
-    return (<$Project>Project</$Project>);
+  return (
+    <$Project>
+      <InProgress />
+    </$Project>
+  );
 }
