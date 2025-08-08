b239c8c25ad16d0914b0b7b3e835f460f99e003b
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2025-08-08 15:53:48 +0200

    eslint for Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { $Footer } from "./Footer.styles.jsx";
 
+/* eslint-disable react/jsx-pascal-case */
 export default function Footer() {
   const { t } = useTranslation();
 
